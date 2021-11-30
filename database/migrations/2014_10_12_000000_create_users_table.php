dba942250301a0b06a1c156e8341aad1f596a944
Oneness Techs <77039268+rajeev-oneness@users.noreply.github.com>
2021-11-30 12:13:40 +0530

    basic Models and migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('role_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image')->default('images/user.jpg');
+            $table->string('otp',10);
             $table->rememberToken();
-            $table->timestamps();
+            $table->string('gender',20)->comment('Male,Female,Not specified');
+            $table->date('dob');
+            $table->string('marital',20)->comment('Single,Married,Divorced');
+            $table->date('aniversary');
+            $table->tinyInteger('is_verified')->default(0);
+            $table->tinyInteger('status')->default(1);
+            $table->softDeletes();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
---
+++ database/migrations/2021_11_30_063158_create_user_roles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->softDeletes();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+
+        $data = [
+            ['name' => 'Admin'],
+            ['name' => 'Business'],
+            ['name' => 'Customer'],
+        ];
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_roles');
+    }
+}
