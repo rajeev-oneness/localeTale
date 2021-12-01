e9faf5ea30cd028d2b590b9d53abc1c1e25ac470
Oneness Techs <77039268+rajeev-oneness@users.noreply.github.com>
2021-12-01 18:42:59 +0530

    Design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,83 +1,73 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>{{ config('app.name', 'localTale') }} - @yield('title')</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('design/css/bootstrap-4.6.0.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('design/css/custom.css')}}">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <!-- loader -->
+    <div class="loading-data" style="display:block; color: #fff;">Loading&#8230;</div>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+    <!-- Header Content -->
+    @include('layouts.header')
 
-                    </ul>
+    <!-- Main Content -->
+    @yield('content')
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+    <!-- Footer Content -->
+    @include('layouts.footer')
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+    <script src="{{asset('design/js/jquery-3.6.0.js')}}"></script>
+    <script src="{{asset('design/js/bootstrap4.6.1.bundle.min.js')}}"></script>
+    <script src="{{asset('design/js/sweetalert.min.js')}}"></script>
+    <script src="{{asset('design/js/jquery.sumoselect.min.js')}}"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.loading-data').hide();
+            $(document).on('submit', 'form', function() {
+                $('button').attr('disabled', 'disabled');
+                $('.loading-data').show();
+            });
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+            @if(Session::has('Success'))
+                toastFire('success', '{{Session::get('success')}}');
+            @elseif(Session::has('Errors'))
+                toastFire('danger', '{{Session::get('success')}}');
+            @endif
+        });
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        // toast fires | type = success, info, danger, warning
+        function toastFire(type='success', title, body='') {
+            $icon = 'check';
+            if (type == 'info') {
+                $icon = 'info-circle';
+            } else if (type == 'danger') {
+                $icon = 'times';
+            } else if (type == 'warning') {
+                $icon = 'exclamation';
+            }
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+            $(document).Toasts('create', {
+                class: 'bg-'+type,
+                title: title,
+                autohide: true,
+                delay: 3000,
+                icon: 'fas fa-'+$icon+' fa-lg',
+                // body: body
+            });
+        }
+
+        
+        function isNumberKey(evt){
+            if(evt.charCode >= 48 && evt.charCode <= 57 || evt.charCode <= 43){  
+                return true;  
+            }  
+            return false;  
+        }
+    </script>
 </body>
 </html>
