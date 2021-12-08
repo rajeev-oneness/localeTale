ba6ace3e9f9f22b5b8eb04c1bb6e9da8742d175f
Oneness Techs <77039268+rajeev-oneness@users.noreply.github.com>
2021-12-08 19:07:02 +0530

    user Authorised deviccd

--- database/migrations/2021_12_03_192249_add_new_columnon_o_auth_access_token_tableon3dec2021722_p_m.php
+++ database/migrations/2021_12_03_192249_add_new_columnon_o_auth_access_token_tableon3dec2021722_p_m.php
@@ -16,6 +16,7 @@ class AddNewColumnonOAuthAccessTokenTableon3dec2021722PM extends Migration
         Schema::table('oauth_access_tokens', function (Blueprint $table) {
             $table->string('device_type')->after('revoked')->comment('ios,android,web');
             $table->string('device_token')->after('device_type')->comment('Device Token will stored here');
+            $table->tinyInteger('authorised')->default(0)->comment('Authorised Device or not');
         });
     }
 
